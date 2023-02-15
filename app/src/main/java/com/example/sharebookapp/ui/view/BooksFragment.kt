cafd52f2d0a3a6b17c6e0d0563e1d728edc87446
Amir0701 <amirusmonov2002@gmail.com>
2023-02-15 21:43:19 +0300

    feature/navigate to NewPublicationFragment

--- app/src/main/java/com/example/sharebookapp/ui/view/BooksFragment.kt
+++ app/src/main/java/com/example/sharebookapp/ui/view/BooksFragment.kt
@@ -6,7 +6,10 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.findFragment
+import androidx.navigation.fragment.findNavController
 import com.example.sharebookapp.R
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class BooksFragment : Fragment() {
     override fun onCreateView(
@@ -16,5 +19,13 @@ class BooksFragment : Fragment() {
         Log.i("Frag", "Book Fragment")
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_books, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val addPublication = view.findViewById<FloatingActionButton>(R.id.addPublicationButton)
+        addPublication.setOnClickListener {
+            findNavController().navigate(R.id.action_booksFragment_to_newPublicationFragment)
+        }
     }
 }
