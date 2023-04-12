801f452326e8e7532ec2633b40db0642e341c5ae
Amir0701 <amirusmonov2002@gmail.com>
2023-04-12 14:00:09 +0300

    bug fix/added ImageRepositoryScope

--- app/src/main/java/com/example/sharebookapp/ioc/component/MyBooksActivityComponent.kt
+++ app/src/main/java/com/example/sharebookapp/ioc/component/MyBooksActivityComponent.kt
@@ -1,9 +1,6 @@
 package com.example.sharebookapp.ioc.component
 
-import com.example.sharebookapp.ioc.scope.CategoryRepositoryScope
-import com.example.sharebookapp.ioc.scope.CityRepositoryScope
-import com.example.sharebookapp.ioc.scope.MyBooksScope
-import com.example.sharebookapp.ioc.scope.PublicationRepositoryScope
+import com.example.sharebookapp.ioc.scope.*
 import com.example.sharebookapp.ui.view.MyBooksActivity
 import dagger.Subcomponent
 
@@ -12,6 +9,7 @@ import dagger.Subcomponent
 @PublicationRepositoryScope
 @CategoryRepositoryScope
 @CityRepositoryScope
+@ImageRepositoryScope
 interface MyBooksActivityComponent {
     fun getMyBooksFragmentComponent(): MyBooksFragmentComponent
     fun inject(myBooksActivity: MyBooksActivity)
