e9a2fde27df4715b3203b2ab93b0a3006cd4acf0
Amir0701 <amirusmonov2002@gmail.com>
2023-09-19 19:42:37 +0300

    placeholder refactoring

--- app/src/main/java/com/example/sharebookapp/util/Utils.kt
+++ app/src/main/java/com/example/sharebookapp/util/Utils.kt
@@ -4,7 +4,7 @@ class Utils {
     companion object {
 //        const val BASE_URL = "http://192.168.0.102:8081"
 
-        const val BASE_URL = "http://192.168.1.4:8081"
+        const val BASE_URL = "http://192.168.87.143:8081"
         //const val BASE_URL = "http://192.168.0.138:8081"
         const val PREF_NAME = "Account"
     }
